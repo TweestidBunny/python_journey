e3d60ac24ca5a41d6344c251b190f7c545844453
Sean <tweestidbunny@gmail.com>
2024-08-31 14:46:24 -0400

    Complete Day 1 - Band Name Generator

---
+++ day1/day1Project/main.py
@@ -0,0 +1,6 @@
+print('Welcome to the Band Name Generator')
+
+name = input('What\'s the name of your home town? ')
+petName = input('What\'s your pet\'s name? ')
+
+print(f"The name of your band could be {name} {petName}")
