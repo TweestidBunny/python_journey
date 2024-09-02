5ff46a1cee21d9eb5da3d227b8ef157c36e895d9
Sean <tweestidbunny@gmail.com>
2024-09-02 12:53:36 -0400

    Remove comments from start of code

--- day5/day5Project/main.py
+++ day5/day5Project/main.py
@@ -1,11 +1,5 @@
 import random
 
-# Password Generator
-
-# Append all into a list, then use the random shuffle.
-# Check to make sure the very first character is a letter.
-# If not, shuffle again.
-
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '?', '-', '+']
